0b271a20653c1d38ceb6749bef572393a8b63583
lucashenriquejbe <lucashenriquejbe@gmail.com>
2015-09-16 13:23:07 -0300

    Algumas atualizações nos modelos e daos, além da criação de alguns servicos.

--- src/main/java/br/ufc/petsi/dao/AgendaDAO.java
+++ src/main/java/br/ufc/petsi/dao/AgendaDAO.java
@@ -6,6 +6,7 @@ import br.ufc.petsi.model.Agenda;
 
 public interface AgendaDAO {
 	public void save(Agenda ag);
+	public void update(Agenda ag);
 	public Agenda getAgendaById(long id);
-	public List<Agenda> getAgendaListByUserId(long idUser);
+	public Agenda getAgendaByUserId(long idUser);
 }
