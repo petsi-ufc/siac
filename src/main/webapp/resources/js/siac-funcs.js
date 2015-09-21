48e65a5f4a7a989c7b1516480f62240dcdf4dbd4
flavio-barros <junior_barros3@hotmail.com>
2015-09-21 10:27:40 -0300

    Alterações no calendário

--- src/main/webapp/resources/js/siac-funcs.js
+++ src/main/webapp/resources/js/siac-funcs.js
@@ -3,18 +3,46 @@
  */
 
 $("document").ready(function(){
-	
+
 	//Quando o usuário clicar na imagem o modal aparece...
 	$("#avatar-img").click(function(){
 		$("#modal-config").modal('show');
 	});
-	
+
 	$("#calendar").fullCalendar({
-		dayClick: function(date, jsEvent, view){
-			$(this).css('background-color', '#2887ED');
-		}
+		header: {
+			left: 'prev',
+			center: 'title',
+			right: 'next'
+		},
+		events: [
+		         {
+		        	 title: 'Consulta Psicologia',
+		        	 start: '2015-09-21',
+		        	 end: '2015-09-21',
+		        	 color: 'red'
+		         },
+		         {
+		        	 title: 'Consulta Nutrição',
+		        	 start: '2015-09-25',
+		        	 end: '2015-09-25',
+		        	 color: 'blue'
+		         },
+		         {
+		        	 title: 'Consulta Psiquiatria',
+		        	 start: '2015-09-29',
+		        	 end: '2015-09-29',
+		        	 color: 'yellow'
+		         }
+
+		         ],
+		         businessHours: true,
+		         editable: false,
+		         dayClick: function(date, jsEvent, view){
+		        	 $(this).css('background-color', '#2887ED');
+		         }
 	});
-	
+
 });
 
 
