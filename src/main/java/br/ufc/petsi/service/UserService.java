61cb42ffa8cef3b27c9c7299c02a9227cfedb0f3
lucashenriquejbe <lucashenriquejbe@gmail.com>
2015-09-23 12:42:30 -0300

    restante dos serviços

--- src/main/java/br/ufc/petsi/service/UserService.java
+++ src/main/java/br/ufc/petsi/service/UserService.java
@@ -1,6 +1,7 @@
 package br.ufc.petsi.service;
 
 import br.ufc.petsi.model.Consultation;
+import br.ufc.petsi.model.Rating;
 import br.ufc.petsi.model.User;
 
 public class UserService {
@@ -9,8 +10,8 @@ public class UserService {
 		return new AgendaService().schedule(user, con);
 	}
 	
-	public boolean rateConsultation(User user, Consultation con) {
-		return new ConsultationService().rate(user, con);
+	public boolean rateConsultation(Consultation con, Rating rating) {
+		return new ConsultationService().rate(con, rating);
 	}
 	
 	public boolean cancelConsultation(Consultation con) {
---
+++ src/test/java/br/ufc/petsi/service/ServiceTest.java
@@ -0,0 +1,5 @@
+package br.ufc.petsi.service;
+
+public class ServiceTest {
+
+}
