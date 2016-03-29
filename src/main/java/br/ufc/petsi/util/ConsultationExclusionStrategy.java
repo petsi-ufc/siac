4ba7120cc1e93efca834e3bbb3fc567c05808a43
Daniel <dielfilho@hotmail.com>
2016-03-29 01:06:44 -0300

    Função de preencher o calendário com as consultas,

---
+++ src/main/java/br/ufc/petsi/util/ConsultationExclusionStrategy.java
@@ -0,0 +1,23 @@
+package br.ufc.petsi.util;
+import java.util.List;
+
+import br.ufc.petsi.model.Professional;
+import br.ufc.petsi.model.SocialService;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+
+
+public class ConsultationExclusionStrategy implements ExclusionStrategy{
+
+	@Override
+	public boolean shouldSkipClass(Class<?> clas) {
+		return clas == List.class || clas == Professional.class || clas == SocialService.class;
+	}
+
+	@Override
+	public boolean shouldSkipField(FieldAttributes attr) {
+		return false;
+	}
+
+}
